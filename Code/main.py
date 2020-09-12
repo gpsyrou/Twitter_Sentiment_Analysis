c76731900b0d16089f6fd6adb1708a369c093330
gpsyrou <gspyrou@umich.edu>
2020-09-12 11:09:09 +0100

    update main

--- Code/main.py
+++ Code/main.py
@@ -11,6 +11,8 @@
 # Import dependenciescle
 import pandas as pd
 import pickle
+import json
+import os
 
 from collections import Counter
 
@@ -31,16 +33,24 @@ from nltk import word_tokenize
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
+json_loc = r'D:\GitHub\Projects\Twitter_Project\Twitter_Topic_Modelling\twitter_config.json'
+
+with open(json_loc) as json_file:
+    configFile = json.load(json_file)
+
+# Project folder location and keys
+os.chdir(configFile["project_directory"])
+
 import Code.twitter_custom_functions as tcf
 
-all_tweets_list_file_loc = r'D:\GitHub\Projects\Twitter_Project\Twitter_Topic_Modelling\all_tweets_list.txt'
+all_tweets_list_file_loc = r'all_tweets_list.txt'
 
 with open (all_tweets_list_file_loc, 'rb') as file:
     allTweetsList = pickle.load(file)
 
 
-# Create a dataframe based on the relevant data from the full list of received
-# tweets
+# Create a dataframe based on the relevant data from the full list of the 
+# received tweets
 user_ls, userid_ls, tweet_ls = [], [], []
 location_ls, datetime_ls, replyto_ls = [], [], []
 geo_loc_ls = []
@@ -57,21 +67,24 @@ for tweet_dict in allTweetsList:
 # Dataframe that contains the data for analysis
 # Note: The twitter API functionality is very broad in what data we can analyse
 # This project will focus on tweets and with their respective location/date.
-df = pd.DataFrame(list(zip(user_ls, userid_ls, tweet_ls,
+tweets_df = pd.DataFrame(list(zip(user_ls, userid_ls, tweet_ls,
                            replyto_ls, location_ls, datetime_ls, geo_loc_ls)),
                   columns=['Username', 'UserID', 'Tweet',
                            'Reply_to', 'Location', 'Date', 'Coordinates'])
 
 # Remove tweets that they did not have any text
-df = df[df['Tweet'].notnull()].reset_index()
-df.drop(columns=['index'], inplace = True)
+tweets_df = tweets_df[tweets_df['Tweet'].notnull()].reset_index()
+tweets_df.drop(columns=['index'], inplace=True)
 
 
 # Detect language and translate if necessary
-df['Tweet'] = df['Tweet'].apply(lambda text: tcf.translateTweet(text))
+tweets_df['Tweet'] = tweets_df['Tweet'].apply(lambda text: tcf.translateTweet(text))
 
-''' USE THE BELOW IF WE WANT TO AVOID RE-TRANSLATING
-df = pd.read_csv('tweetsdata20200314.csv', sep='\t',
+''' 
+tweets_df.to_csv('full_tweets_20200912.csv', sep='\t', encoding='utf-8', index=False)
+
+USE THE BELOW IF WE WANT TO AVOID RE-TRANSLATING
+tweets_df = pd.read_csv('full_tweets_20200912.csv', sep='\t',
                  encoding = 'utf-8', index_col=0)
 '''
 
@@ -88,15 +101,15 @@ geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1,
 # Split it in batches and identify the locations
 step = 100
 
-for batch in range(0, df.shape[0], step):
+for batch in range(0, tweets_df.shape[0], step):
     batchstep = batch+step
-    if batchstep > df.shape[0]:
-        batchstep = batch + (df.shape[0]%step)
+    if batchstep > tweets_df.shape[0]:
+        batchstep = batch + (tweets_df.shape[0]%step)
     print(f'\nCalculating batch: {batch}-{batchstep}\n')
-    df['Point'] = df['Location'][batch:batchstep].apply(lambda 
+    tweets_df['Point'] = tweets_df['Location'][batch:batchstep].apply(lambda 
                                    loc: tcf.getValidCoordinates(loc, geolocator))
 
-dfWithCoords = df[df['Point'].notnull()]
+dfWithCoords = tweets_df[tweets_df['Point'].notnull()]
 dfWithCoords['Latitude'] = dfWithCoords['Point'].apply(lambda x: x[0])
 dfWithCoords['Longitude'] = dfWithCoords['Point'].apply(lambda x: x[1])
 
@@ -116,11 +129,11 @@ commonTweeterStopwords = ['rt', 'retweet', 'new', 'via', 'us', 'u', '2019',
 allStopWords.extend(commonTweeterStopwords + spanish_stopwords)
 num_list = '0123456789'
 
-df['Tweet'] = df['Tweet'].apply(lambda x: tcf.rmPunctAndStopwords(x, allStopWords, num_list))
+tweets_df['Tweet'] = tweets_df['Tweet'].apply(lambda x: tcf.rmPunctAndStopwords(x, allStopWords, num_list))
 
 # Find the most common words across all tweets
 
-tweet_list = list([x.split() for x in df['Tweet'] if x is not None])
+tweet_list = list([x.split() for x in tweets_df['Tweet'] if x is not None])
 all_words_counter = Counter(x for xs in tweet_list for x in set(xs))
 all_words_counter.most_common(20)
 
@@ -137,7 +150,7 @@ tcf.plotMostCommonWords(mostCommontweets)
 # 2. WordCloud vizualisation
 
 plt.figure(figsize=(10, 10))
-gen_text = ' '.join([x for x in df['Tweet'] if x is not None])
+gen_text = ' '.join([x for x in tweets_df['Tweet'] if x is not None])
 wordcloud = WordCloud().generate(gen_text)
 
 plt.imshow(wordcloud, interpolation='bilinear')
@@ -147,7 +160,7 @@ plt.show()
 # 3. Find bigrams (pairs of words that frequently appear next to each other)
 
 # First convert the list of tweets into one consecutive string
-allTweetsString = ' '.join([x for x in df['Tweet']])
+allTweetsString = ' '.join([x for x in tweets_df['Tweet']])
 
 bigram_measures = BigramAssocMeasures()
 
@@ -182,7 +195,7 @@ plt.show()
 # 4. Sentiment analysis on tweets based on Liu Hu opinion lexicon
 # Classify tweets as 'positive', 'negative' or 'neutral' based on the polarity
 # of the words present in a sentence.
-sentimentDF = df.copy()
+sentimentDF = tweets_df.copy()
 
 sentimentDF['Sentiment'] = sentimentDF['Tweet'].apply(lambda tweet:
                                                       tcf.liu_hu_opinion_lexicon(tweet))

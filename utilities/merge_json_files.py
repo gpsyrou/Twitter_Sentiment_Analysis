6186733408c90ccccd59e2f7b472c934e16acd39
gpsyrou <gspyrou@umich.edu>
2020-09-27 10:30:38 +0100

    k

--- utilities/merge_json_files.py
+++ utilities/merge_json_files.py
@@ -11,13 +11,11 @@ import json
 import pickle
 
 # Secure location of the required keys to connect to the API
-# This config also contains the search query (in this case 'coronavirus')
 json_loc = r'D:\GitHub\Projects\Twitter_Project\Twitter_Topic_Modelling\twitter_config.json'
 
 with open(json_loc) as json_file:
     config = json.load(json_file)
 
-# Project folder location and keys
 os.chdir(config["project_directory"])
 
 import twitter_custom_functions as tcf
@@ -26,24 +24,21 @@ import twitter_custom_functions as tcf
 jsonl_files_folder = os.path.join(config["project_directory"],
                                   config["outputFiles"])
 
-# List that will contain all the Tweets that we managed to receive
-# via the use of the API
-
-allTweetsList = []
+all_tweets = []
 
 print('Merging the json files...')
 
 for file in os.listdir(jsonl_files_folder):
     if 'twitter' in file:
-        tweets_full_list = tcf.load_jsonl_data(os.path.join(jsonl_files_folder,
+        tweet_batch = tcf.load_jsonl_data(os.path.join(jsonl_files_folder,
                                                           file))
-        allTweetsList += tweets_full_list
+        all_tweets += tweet_batch
         
 print('Merging completed...')
 
 # Pickle the list to a common .txt file
 out_filename = 'all_tweets_list.txt'
 with open(out_filename, 'wb') as file:
-    pickle.dump(allTweetsList, file)
+    pickle.dump(all_tweets, file)
 
-print(f'The text file has saved {int(len(allTweetsList))} tweets in {out_filename}')
+print(f'The text file has saved {int(len(all_tweets))} tweets in {out_filename}')

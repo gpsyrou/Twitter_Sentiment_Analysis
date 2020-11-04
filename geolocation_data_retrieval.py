cdc921c93b9fe9c7b8ab05d2b20a97cd243eed20
gpsyrou <gspyrou@umich.edu>
2020-11-04 21:02:01 +0000

    make the script executable

--- geolocation_data_retrieval.py
+++ geolocation_data_retrieval.py
@@ -5,11 +5,15 @@
 -- Last Updated:  03/11/2020
 -------------------------------------------------------------------
 """
+import os
+import json
+import argparse
 
 import pandas as pd
 import numpy as np
 from datetime import datetime
 import time
+
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.exc import GeocoderTimedOut
@@ -18,14 +22,24 @@ from sentiment_class import month_as_string
 import utilities.plot_world_map as pmap
 from sentiment_class import TwitterSentiment, month_as_string
 
+json_loc = r'D:\GitHub\Projects\Twitter_Project\Twitter_Topic_Modelling\twitter_config.json'
+
+with open(json_loc) as json_file:
+    config = json.load(json_file)
+
+# Project folder location and keys
+os.chdir(config["project_directory"])
+
 geolocator = Nominatim(user_agent="https://developer.twitter.com/en/apps/17403833") 
    
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1, max_retries=3, error_wait_seconds=2)
 
 translated_tweets_filename = 'tweets_translated.csv'
 
-input_year = 2020
-input_month = 2
+parser=argparse.ArgumentParser()
+parser.add_argument('input_year', type=int)
+parser.add_argument('input_month', type=int)
+args = parser.parse_args()
 
 def get_valid_coordinates(location: str, geolocator: Nominatim) -> list:
     """
@@ -54,7 +68,7 @@ tweets_df = pd.read_csv(translated_tweets_filename, sep='\t', encoding = 'utf-8'
 tweets_df = tweets_df[tweets_df['Tweets_Clean'].notnull()].reset_index()
 
 df_subset = TwitterSentiment(input_df=tweets_df, tweet_column='Tweets_Clean')
-df_subset.subset_dataframe(year=input_year, month=input_month)
+df_subset.subset_dataframe(year=args.input_year, month=args.input_month)
 
 df_with_coordinates = df_subset.df
 df_with_coordinates.reset_index(drop=True, inplace=True)
@@ -76,5 +90,5 @@ for i in range(0, df_with_coordinates.shape[0]):
     df_with_coordinates.loc[i, 'Longitude'] = longitude
     print('Location found in: [{0}, {1}]'.format(latitude, longitude))
 
-month = month_as_string(input_month)
-df_with_coordinates.to_csv(f'tweets_with_geolocation_{month}_{input_year}.csv', sep='\t', encoding='utf-8', index=False)
+month = month_as_string(args.input_month)
+df_with_coordinates.to_csv(f'tweets_with_geolocation_{month}_{args.input_year}.csv', sep='\t', encoding='utf-8', index=False)
